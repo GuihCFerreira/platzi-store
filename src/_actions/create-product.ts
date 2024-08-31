f70c627a017aedf24a1d1597e219fbd5e28cf8f3
GuihCFerreira <guihhh231@gmail.com>
2024-08-31 09:47:54 -0300

    fix: atualizado a interface de criar produto

--- src/_actions/create-product.ts
+++ src/_actions/create-product.ts
@@ -5,11 +5,11 @@ import { revalidatePath } from "next/cache";
 
 export interface CreateProductProps {
   product: {
-    title: string | null;
-    description: string | null;
-    price: number | null;
-    categoryId: number | null;
-    images: string[] | null;
+    title?: string;
+    description?: string;
+    price?: number;
+    categoryId?: number;
+    images?: string[];
   };
 }
 
